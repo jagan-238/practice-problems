dddc648914ed195a7a8e1672f8d6131af9ec4e5f
jagan-238 <eragalajaganmohanreddy1160.com>
2026-04-17 12:47:36 +0530

    push

--- Elevate-2.0/day1/index.js
+++ Elevate-2.0/day1/index.js
@@ -1,11 +1,11 @@
-const products = [
-  ['Electronics', 'Laptop', 999],
-  ['Clothing', 'Shirt', 29],
-  ['Electronics', 'Mouse', 25],
-  ['Clothing', 'Pants', 49],
-  ['Electronics', 'Keyboard', 75],
-  ['Clothing', 'Jacket', 89]
-];
+// const products = [
+//   ['Electronics', 'Laptop', 999],
+//   ['Clothing', 'Shirt', 29],
+//   ['Electronics', 'Mouse', 25],
+//   ['Clothing', 'Pants', 49],
+//   ['Electronics', 'Keyboard', 75],
+//   ['Clothing', 'Jacket', 89]
+// ];
 
 
 // const result = products.reduce((acc, [category, name, price]) => {
@@ -24,3 +24,62 @@ const products = [
 
 
 
+
+const users = [
+  {
+    id: 1,
+    name: 'Alice',
+    courses: [
+      { title: 'React', rating: 4.5, completed: true },
+      { title: 'Node', rating: 3.8, completed: true },
+      { title: 'CSS', rating: 4.2, completed: true }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    courses: [
+      { title: 'HTML', rating: 3.5, completed: true },
+      { title: 'JavaScript', rating: 4.1, completed: false },
+      { title: 'MongoDB', rating: 4.3, completed: true }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Charlie',
+    courses: [
+      { title: 'React', rating: 4.8, completed: true },
+      { title: 'Node', rating: 4.4, completed: true },
+      { title: 'Express', rating: 4.0, completed: true }
+    ]
+  },
+  {
+    id: 4,
+    name: 'David',
+    courses: [
+      { title: 'CSS', rating: 3.9, completed: true },
+      { title: 'HTML', rating: 4.2, completed: false },
+      { title: 'JS', rating: 4.0, completed: true }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Eve',
+    courses: [
+      { title: 'React', rating: 4.6, completed: true },
+      { title: 'Redux', rating: 4.7, completed: true },
+      { title: 'TypeScript', rating: 4.5, completed: true }
+    ]
+  }
+];
+
+const result = users.filter(user => {
+    const validCourses = user.courses.filter(course => course.completed && course.rating > 4);
+    return validCourses.length >= 2 ;
+}) 
+
+.map(user => ({
+    id: user.id,
+    name: user.name
+}));
+console.log(result);
